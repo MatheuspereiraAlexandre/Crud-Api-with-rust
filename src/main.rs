3f5649db6f828d66c6e9b48549f292bb626ecd29
MatheuspereiraAlexandre <matheusperaalexandre@gmail.com>
2025-08-12 14:42:06 -0300

    WIP: mongo db connection

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use axum::Json;
+use axum::Json; // json. apenas json.
 use axum::extract::State;
 use axum::{Router, routing::get, routing::post}; // importa coisas
+use mongodb::{Client, Collection, options::ClientOptions};
 use serde::{Deserialize, Serialize};
+use std::fmt::format;
 use std::sync::Arc;
-use std::sync::Mutex; 
+use std::sync::Mutex; // mongo db adicionado no caso a importação
 
-
-#[allow(dead_code)]
+#[allow(dead_code)] // isso daqui é pra parar a frecsura do erro do crlh
 #[derive(Debug, Deserialize)] // permite transformar o json em struct rust coisas loucas essas
 struct CreateUser {
-    name: String,
-    email: String,
+    name: String,  // usa string
+    email: String, // e tbm usa string
 }
 
 #[derive(Debug, Serialize)] // converte struct para json na hora de devolver essa merda
@@ -21,12 +22,18 @@ struct ApiResponse {
 
 #[derive(Default)]
 struct AppState {
-    users: Vec<CreateUser>,
+    user_collection: Collection<CreateUser>, // v
 }
 
 #[tokio::main] // define o tokio q é pra usar async
 async fn main() {
-    let state = Arc::new(Mutex::new(AppState::default())); // evita uso desnecessario de threads
+    let client_options = ClientOptions::parse("mongodb://localhost:27017")
+        .await
+        .unwrap();
+    let client = Client::with_options(client_options).unwrap();
+    let database = client.database("CrudRust");
+    let user_collection = database.collection::<CreateUser>("users");
+    let state = Arc::new(Mutex::new(AppState { user_collection }));
 
     // construindo uma rota
     let app: Router = Router::new()
@@ -44,11 +51,8 @@ async fn create_user(
     State(state): State<Arc<Mutex<AppState>>>,
     Json(payload): Json<CreateUser>,
 ) -> Json<ApiResponse> {
-    let mut app_state = state.lock().unwrap();
-    app_state.users.push(payload);
-
-    Json(ApiResponse {
-        success: true,
-        message: "".into(),
-    })
+    match state.user_collection.insert_one(payload, none).await {
+        Ok(_) => Json(ApiResponse {success: true, message: "usuario foi colocado no bd".into() }),
+        Err(err) => Json(ApiResponse {success: false, message: format!("erro {}", err)}),
+    }
 }
