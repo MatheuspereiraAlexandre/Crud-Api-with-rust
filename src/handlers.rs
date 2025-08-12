fefa205e18c74c89b5c596de36022249b0fbc8a7
MatheuspereiraAlexandre <matheusperaalexandre@gmail.com>
2025-08-12 17:04:50 -0300

    WIP: Delete Method

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,3 +1,4 @@
+use mongodb::bson::doc;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
@@ -17,6 +18,16 @@ pub struct CreateUserResponse {
     pub message: String,
 }
 
+pub struct DeleteUserRequest {
+    pub name: String,
+    pub email: String,
+}
+
+pub struct DeleteUserResponse {
+    pub success: bool,
+    pub message: String,
+}
+
 pub async fn create_user(
     State(state): State<Arc<Mutex<AppState>>>,
     Json(payload): Json<CreateUserRequest>,
@@ -28,6 +39,27 @@ pub async fn create_user(
             message: "usuario foi colocado no bd".into(),
         }),
         Err(err) => Json(CreateUserResponse {
+            success: false,
+            message: format!("erro {}", err),
+        }),
+    }
+}
+
+pub async fn delete_user(
+    State(state): State<Arc<Mutex<AppState>>>,
+    Json(payload): Json<DeleteUserRequest>,
+) -> Json<DeleteUserResponse> {
+    let state = state.lock().await;
+
+    let filtrer = doc! {"name": &payload.name, "email": &payload.email, };
+    match state.db.delete_one(filtrer).await {
+        Ok(_) => Json(
+            (DeleteUserResponse {
+                success: true,
+                message: "usuario deltado com sucesso".into(),
+            }),
+        ),
+        Err(err) => Json(DeleteUserResponse {
             success: false,
             message: format!("erro {}", err),
         }),
