c77cb5d033b58bc0a371d6b7c42f5d4b878a6bf9
matheus alexandre <matheusperaalexandre@gmail.com>
2025-10-29 19:51:20 -0300

    Refactor init_db to use MONGO_URI from env
    Refactor database initialization to use environment variable for MONGO_URI, with a fallback to localhost.

--- src/db.rs
+++ src/db.rs
@@ -1,9 +1,18 @@
 use mongodb::{Client, Database, options::ClientOptions};
+use dotenvy::var;
+use std::env;
 
+/// Inicializa o DB usando a variável de ambiente MONGO_URI.
 pub async fn init_db() -> Database {
-    let client_uri = "mongodb+srv://Lume:matheus33%23%24@lume.ipmctxj.mongodb.net/?appName=Lume"; // a url da conexão
-    let options = ClientOptions::parse(client_uri).await.unwrap();
-    let client = Client::with_options(options).unwrap();
+    // Ler URI do env (ex: "MONGO_URI")
+    let client_uri = var("MONGO_URI").unwrap_or_else(|_| {
+        // fallback para dev local
+        "mongodb://localhost:27017".to_string()
+    });
 
-    client.database("Lume") // nome do database
+    let mut options = ClientOptions::parse(&client_uri).await.expect("Erro parse ClientOptions");
+    options.app_name = Some("Lume".to_string());
+    let client = Client::with_options(options).expect("Erro criar mongo client");
+
+    client.database("Lume")
 }
