fab01a497cd32245600c60ad2387f0f83475e78f
matheus alexandre <matheusperaalexandre@gmail.com>
2025-10-29 19:49:11 -0300

    Refactor create_router and update search_user route
    Updated create_router to use Arc<AppState> instead of Arc<Mutex<AppState>> and changed the search_user route to use POST.

--- src/routes.rs
+++ src/routes.rs
@@ -1,20 +1,19 @@
 use std::sync::Arc;
-use tokio::sync::Mutex;
-
 use axum::{
     Router,
     routing::{delete, get, post, put},
 };
-
 use crate::AppState;
-use crate::handlers::{create_user, delete_user, edit_user, health_check, search_user}; // importa os handlers
+use crate::handlers::{create_user, delete_user, edit_user, health_check, search_user};
 
-pub fn create_router(state: Arc<Mutex<AppState>>) -> Router {
+/// Cria o roteador e injeta o estado compartilhado.
+/// Note: agora AppState é Arc<AppState> (sem Mutex)
+pub fn create_router(state: Arc<AppState>) -> Router {
     Router::new()
-        .route("/signUp", post(create_user)) // rota POST para criar user
+        .route("/signUp", post(create_user))
         .route("/health", get(health_check))
         .route("/delete", delete(delete_user))
         .route("/edit", put(edit_user))
-        .route("/find", get(search_user))
-        .with_state(state) // injeta o estado
+        .route("/find", post(search_user))
+        .with_state(state)
 }
